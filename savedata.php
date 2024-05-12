6178121e8e302575a001c10b682c0d1ca9244f03
Temiraliyev <156788797+temiraliyevm@users.noreply.github.com>
2024-05-12 14:21:04 +0500

    sign up page done

---
+++ savedata.php
@@ -0,0 +1,22 @@
+<?php
+require "config.php";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+//   echo "Connected successfully" . '<br>';
+$emailadress = $_POST["emailadress"];
+$passwords = $_POST["passwords"];
+// $email = $_POST["email"];
+// sql to insert data
+$sql = "INSERT INTO authorization (emailadress, passwords)
+VALUES ('$emailadress', '$passwords')";
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+    
+    $conn->close();
+?>
